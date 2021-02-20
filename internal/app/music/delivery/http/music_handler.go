1812e176d874179dae62d91610bb236f0b8cc4e2
Andrew <a-kireev1989@mail.ru>
2021-02-20 19:45:32 +0300

    Добавил логирование в music_handler

--- internal/app/music/delivery/http/music_handler.go
+++ internal/app/music/delivery/http/music_handler.go
@@ -32,6 +32,10 @@ func NewMusicHandler(usecase music.Usecase) *MusicHandler {
 		logger:         logrus.New(),
 		sessionsClient: client.NewSessionsClient(grpcCon),
 	}
+	err = ConfigLogger(handler)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	handler.router.HandleFunc("/getMusic", handler.GetMusic)
 	handler.router.HandleFunc("/createSession", handler.CreateSession)
@@ -55,7 +59,7 @@ func (handler *MusicHandler) CreateSession(w http.ResponseWriter, r *http.Reques
 	session, err := handler.sessionsClient.Create(context.Background(), userID)
 	fmt.Println("Result: = " + session.Status)
 	if err != nil {
-		fmt.Println(err)
+		handler.logger.Errorf("Error in creating session: %v", err)
 	}
 
 	cookie := &http.Cookie{
@@ -74,7 +78,7 @@ func (handler *MusicHandler) CheckSession(w http.ResponseWriter, r *http.Request
 	sessionID, err := r.Cookie("session_id")
 
 	if err != nil {
-		fmt.Println(err)
+		handler.logger.Errorf("Error in parsing cookie: %v", err)
 		w.Write([]byte("Куки нет, нужно редиректнуть на авторизацию"))
 		return
 	}
@@ -82,14 +86,11 @@ func (handler *MusicHandler) CheckSession(w http.ResponseWriter, r *http.Request
 	userID, _ := strconv.Atoi(sessionID.Value)
 	session, err := handler.sessionsClient.Check(context.Background(), userID)
 	fmt.Println("Result: = " + session.Status)
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	if err == nil && session.ID == userID {
 		w.Write([]byte("Куку есть и id у нее = " + strconv.Itoa(session.ID)))
 	} else {
-		fmt.Println(err)
+		handler.logger.Errorf("Error in checking session: %v", err)
 		w.Write([]byte("Куки нет"))
 	}
 }
@@ -99,7 +100,7 @@ func (handler *MusicHandler) DeleteSession(w http.ResponseWriter, r *http.Reques
 	session, err := r.Cookie("session_id")
 
 	if err != nil {
-		fmt.Println(err)
+		handler.logger.Errorf("Error in parsing cookie: %v", err)
 		return
 	}
 
@@ -108,7 +109,7 @@ func (handler *MusicHandler) DeleteSession(w http.ResponseWriter, r *http.Reques
 	result, err := handler.sessionsClient.Delete(context.Background(), sessionID)
 	fmt.Println("Result: = " + result.Status)
 	if err != nil {
-		fmt.Println(err)
+		handler.logger.Errorf("Error in deleting session: %v", err)
 		w.Write([]byte("some error happened(("))
 	} else {
 		w.Write([]byte("cookie with id = " + session.Value + " was deleted"))
@@ -116,6 +117,16 @@ func (handler *MusicHandler) DeleteSession(w http.ResponseWriter, r *http.Reques
 		session.Expires = time.Now().AddDate(0, 0, -5)
 		http.SetCookie(w, session)
 	}
+}
+
+func ConfigLogger(handler *MusicHandler) error {
+	//level, err := logrus.ParseLevel(logrus.DebugLevel)
+	//if err != nil {
+	//	return err
+	//}
+
+	handler.logger.SetLevel(logrus.DebugLevel)
+	return nil
 }
 
 func (handler *MusicHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
