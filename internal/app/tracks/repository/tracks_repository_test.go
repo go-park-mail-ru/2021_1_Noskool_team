31df4410cfdc9a6b01a07d207f579bb96f9ff183
Andrew <a-kireev1989@mail.ru>
2021-03-31 14:28:01 +0300

    Тесты на получения треков по альбому, по юзеру

--- internal/app/tracks/repository/tracks_repository_test.go
+++ internal/app/tracks/repository/tracks_repository_test.go
@@ -9,6 +9,27 @@ import (
 	"testing"
 )
 
+var (
+	tracksForTests = []*models.Track{
+		{
+			TrackID:     1,
+			Tittle:      "song",
+			Text:        "sing song song",
+			Audio:       "/api/v1/data/audio/track/2.mp3",
+			Picture:     "picture",
+			ReleaseDate: "2021-03-04",
+		},
+		{
+			TrackID:     2,
+			Tittle:      "helloWorld",
+			Text:        "sing song song ooooo",
+			Audio:       "/api/v1/data/audio/2.mp3",
+			Picture:     "/api/v1/data/audio/tracks/2.mp3",
+			ReleaseDate: "2020-03-04",
+		},
+	}
+)
+
 func TestGetTrackByID(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
@@ -98,6 +119,60 @@ func TestGetTracksByTittle(t *testing.T) {
 	fmt.Println(track)
 	assert.NoError(t, err)
 	if !reflect.DeepEqual(expectedTrack, track[0]) {
+		t.Fatalf("Not equal")
+	}
+}
+
+func TestGetTrackByUserID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock '%s'", err)
+	}
+	tracRep := NewTracksRepository(db)
+
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{
+		"track_id", "tittle", "text", "audio", "picture", "release_date",
+	})
+	for _, row := range tracksForTests {
+		rows.AddRow(row.TrackID, row.Tittle, row.Text,
+			row.Audio, row.Picture, row.ReleaseDate)
+	}
+	query := "SELECT tracks.track_id, tittle, text, audio, picture, release_date from " +
+		"tracks\n\t\t\tLEFT JOIN tracks_to_user"
+	mock.ExpectQuery(query).WithArgs(uint64(1)).WillReturnRows(rows)
+	track, err := tracRep.GetTracksByUserID(1)
+
+	assert.NoError(t, err)
+	if !reflect.DeepEqual(tracksForTests, track) {
+		t.Fatalf("Not equal")
+	}
+}
+
+func TestGetTrackByAlbumID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock '%s'", err)
+	}
+	tracRep := NewTracksRepository(db)
+
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{
+		"track_id", "tittle", "text", "audio", "picture", "release_date",
+	})
+	for _, row := range tracksForTests {
+		rows.AddRow(row.TrackID, row.Tittle, row.Text,
+			row.Audio, row.Picture, row.ReleaseDate)
+	}
+	query := "SELECT tracks.track_id, tittle, text, audio, picture, release_date FROM " +
+		"tracks\n\t\t\tleft join tracks_to_albums"
+	mock.ExpectQuery(query).WithArgs(uint64(1)).WillReturnRows(rows)
+	track, err := tracRep.GetTracksByAlbumID(1)
+
+	assert.NoError(t, err)
+	if !reflect.DeepEqual(tracksForTests, track) {
 		t.Fatalf("Not equal")
 	}
 }
