61cc3ff4729f67b21ff806ecd9ef63bc4c8884f7
Andrew <a-kireev1989@mail.ru>
2021-04-03 21:34:21 +0300

    Написаны тесты на поиск в репозиториях

--- internal/app/album/repository/albums_repository_test.go
+++ internal/app/album/repository/albums_repository_test.go
@@ -9,6 +9,23 @@ import (
 	"testing"
 )
 
+var (
+	expectedAlbums = []*models.Album{
+		{
+			AlbumID:     1,
+			Tittle:      "album1",
+			Picture:     "picture1",
+			ReleaseDate: "date1",
+		},
+		{
+			AlbumID:     2,
+			Tittle:      "album2",
+			Picture:     "picture2",
+			ReleaseDate: "date2",
+		},
+	}
+)
+
 func TestGetAlbumByID(t *testing.T) {
 	dbCon, mock, err := sqlmock.New()
 	if err != nil {
@@ -39,6 +56,35 @@ func TestGetAlbumByID(t *testing.T) {
 	fmt.Println(album)
 	assert.NoError(t, err)
 	if !reflect.DeepEqual(expectedAlbum, album) {
+		t.Fatalf("Not equal")
+	}
+}
+
+func TestSearchAlbums(t *testing.T) {
+	dbCon, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock '%s'", err)
+	}
+	albumsRep := NewAlbumsRepository(dbCon)
+
+	defer dbCon.Close()
+	rows := sqlmock.NewRows([]string{
+		"album_id", "tittle", "picture", "release_date",
+	})
+
+	for _, row := range expectedAlbums {
+		rows.AddRow(row.AlbumID, row.Tittle,
+			row.Picture, row.ReleaseDate)
+	}
+	query := "SELECT"
+
+	mock.ExpectQuery(query).WithArgs("album").WillReturnRows(rows)
+
+	album, err := albumsRep.SearchAlbums("album")
+
+	fmt.Println(album)
+	assert.NoError(t, err)
+	if !reflect.DeepEqual(expectedAlbums, album) {
 		t.Fatalf("Not equal")
 	}
 }
