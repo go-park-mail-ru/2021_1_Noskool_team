aadf39741dce42e45de68c59df53a53c4b9eb244
Steve <amartery@gmail.com>
2021-04-10 10:37:42 +0300

    поменял модельку музыкантов, добавил жанры в бд

--- internal/app/musicians/models/musician.go
+++ internal/app/musicians/models/musician.go
@@ -1,8 +1,9 @@
 package models
 
 type Musician struct {
-	MusicianID  int    `json:"musician_id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Picture     string `json:"picture"`
+	MusicianID  int      `json:"musician_id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Picture     string   `json:"picture"`
+	Genre       []string `json:"genre"`
 }
