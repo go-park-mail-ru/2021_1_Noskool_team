f36254065f4cb5894e4bb3f440e15337f65a45dc
Andrew <a-kireev1989@mail.ru>
2021-05-05 00:00:43 +0300

    Добавил дефолтную авту для плейлистов

--- scripts/music_db.sql
+++ scripts/music_db.sql
@@ -283,9 +283,9 @@ values (1, 1),
        (7, 7);
 
 
-insert into playlists (playlist_id, user_id, tittle, description, picture, release_date)
-VALUES (1, 0, 'Alternative', 'Alternative music', '/api/v1/data/img/playlists/alternative.webp', '2021-06-01'),
-       (2, 0, 'For Good Mood', 'favorite music', '/api/v1/data/img/playlists/happy.webp', '2021-07-01');
+-- insert into playlists (playlist_id, user_id, tittle, description, picture, release_date)
+-- VALUES (1, 0, 'Alternative', 'Alternative music', '/api/v1/data/img/playlists/alternative.webp', '2021-06-01'),
+--        (2, 0, 'For Good Mood', 'favorite music', '/api/v1/data/img/playlists/happy.webp', '2021-07-01');
 
 insert into tracks_to_playlist(track_id, playlist_id)
 VALUES (1, 1),
