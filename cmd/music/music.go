0c2328e3558c6b46f61b1476db796c26d4653a5e
Andrew <a-kireev1989@mail.ru>
2021-05-23 13:59:50 +0300

    страница пользователя

--- cmd/music/music.go
+++ cmd/music/music.go
@@ -15,6 +15,7 @@ import (
 	"2021_1_Noskool_team/internal/pkg/server"
 	"2021_1_Noskool_team/internal/pkg/utility"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/sirupsen/logrus"
 	"time"
 )
@@ -26,10 +27,10 @@ const (
 func main() {
 	time.Sleep(30 * time.Second)
 	config := configs.NewConfig()
-	//_, err := toml.DecodeFile(configPath, config)
-	//if err != nil {
-	//	logrus.Error(err)
-	//}
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		logrus.Error(err)
+	}
 
 	musicDBCon, err := utility.CreatePostgresConnection(config.MusicPostgresBD)
 	if err != nil {
